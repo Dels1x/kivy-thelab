8340e4c6c14b8546b30eabecb0e047db6c00909e
Dels1x <delsixreborn@gmail.com>
2022-04-14 23:05:35 +0300

    Added toggle button for locking the counter, adding/removing 5 instead of 1, added plus and minus features

--- main.py
+++ main.py
@@ -11,24 +11,35 @@ from kivy.uix.widget import Widget
 
 class WidgetsExample(GridLayout):
     count = 0
+    count_enabled = True
     add = 1
     text = StringProperty("0")
-    def on_toggle_button_state(self, widget):
+
+    def on_toggle_5_state(self, widget):
         if widget.state == "down":
-            widget.text = "ON"
+            widget.text = "*5:ON"
             self.add = 5
         else:
-            widget.text = "OFF"
+            widget.text = "*5: OFF"
             self.add = 1
 
+    def on_toggle_enable_state(self, widget):
+        if widget.state == "down":
+            widget.text = "Counter lock: ON"
+            self.count_enabled = False
+        else:
+            widget.text = "Counter lock: OFF"
+            self.count_enabled = True
 
     def on_plus_button_click(self):
-        self.count += self.add
-        self.text = str(self.count)
+        if self.count_enabled:
+            self.count += self.add
+            self.text = str(self.count)
 
     def on_minus_button_click(self):
-        self.count -= self.add
-        self.text = str(self.count)
+        if self.count_enabled:
+            self.count -= self.add
+            self.text = str(self.count)
 
 
 class StackLayoutExample(StackLayout):
