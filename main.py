135c36b3d6cf02e23115a70c00de793c76ab1101
Dels1x <delsixreborn@gmail.com>
2022-04-14 20:54:02 +0300

    01

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from kivy.app import App
 from kivy.metrics import dp
+from kivy.properties import StringProperty
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
+
+
+class WidgetsExample(GridLayout):
+    count = 0
+    text = StringProperty("")
+    def on_button_click(self):
+        self.count += 1
+        self.text = str(self.count)
 
 
 class StackLayoutExample(StackLayout):
